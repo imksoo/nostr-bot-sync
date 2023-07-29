3d9394e98559c0375db273834d5b61afaa8b363f
Kirino Minato <kirino.minato@gmail.com>
2023-07-29 18:00:10 +0000

    Impl.

---
+++ index.ts
@@ -0,0 +1,83 @@
+import "websocket-polyfill";
+import * as Nostr from "nostr-tools";
+
+const SOURCE_RELAY = "wss://relay.nostr.wirednet.jp";
+const DESTINATION_RELAY = "wss://relay-jp.nostr.wirednet.jp";
+const BOT_LIST_PUBKEY = "18c3434bf332fcbede0be65df140f1bf9ad1bbf6c923242b5b4ac798c276a35b";
+
+async function main() {
+  const srcRelay = Nostr.relayInit(SOURCE_RELAY);
+  srcRelay.on("error", () => {
+    console.log("Source relay error");
+    process.exit(1);
+  })
+  srcRelay.on("disconnect", () => {
+    console.log("Source relay disconnected");
+    process.exit(1);
+  });
+  const srcRelayTimeout = setTimeout(() => {
+    console.log("Source relay timeout");
+    process.exit(1);
+  }, 60 * 1000);
+  await srcRelay.connect();
+  clearTimeout(srcRelayTimeout);
+
+  const destRelay = Nostr.relayInit(DESTINATION_RELAY);
+  destRelay.on("error", () => {
+    console.log("Destination relay error");
+    process.exit(1);
+  })
+  destRelay.on("disconnect", () => {
+    console.log("Destination relay disconnected");
+    process.exit(1);
+  });
+  const destRelayTimeout = setTimeout(() => {
+    console.log("Destination relay timeout");
+    process.exit(1);
+  }, 60 * 1000);
+  await destRelay.connect();
+  clearTimeout(destRelayTimeout);
+
+  const pool = new Nostr.SimplePool();
+  let subscribeEvents: Nostr.Sub | undefined;
+
+  duplicateEvents();
+  setInterval(async () => {
+    await duplicateEvents();
+  }, 5 * 60 * 1000);
+
+  async function duplicateEvents() {
+    if (subscribeEvents !== undefined) {
+      subscribeEvents.unsub();
+    }
+
+    let followers: string[];
+    subscribeEvents = pool.sub([SOURCE_RELAY], [{
+      kinds: [3],
+      authors: [BOT_LIST_PUBKEY],
+      limit: 1
+    }]);
+    subscribeEvents.on("event", (event) => {
+      followers = event.tags.filter((t) => (t[0] === "p")).map((t) => (t[1]));
+      console.log("followers", followers);
+      subscribeEvents?.unsub();
+
+      subscribeEvents = pool.sub([SOURCE_RELAY], [{
+        authors: [...(new Set(followers))],
+        since: Math.floor((new Date().getTime() / 1000) - 5 * 60)
+      }]);
+      subscribeEvents.on("event", (event) => {
+        console.log(event);
+        const pub = pool.publish([DESTINATION_RELAY], event);
+        pub.on("ok", ()=>{
+          console.log("OK", event.id);
+        });
+        pub.on("failed", ()=>{
+          console.log("NG", event.id);
+        })
+      })
+    });
+  }
+}
+
+main();
